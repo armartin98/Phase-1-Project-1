0152dd263f7c64e0610fc43706bcf4a76d3a9de9
Andrew <armartin1998@gmail.com>
2023-04-06 21:41:16 -0400

    added currency exchange calaulation

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-function currencyCalculator(){
+function currency(){
     fetch('https://raw.githubusercontent.com/fawazahmed0/currency-api/1/latest/currencies/usd.json')
         .then(res => res.json())
-        .then(rates => console.log(rates))
+        .then(rates => {
+            const exchangeType = document.querySelector('#exchangeType')
+            const ratesArray = Object.keys(rates.usd)
+            ratesArray.map(x => {
+                const option = document.createElement('option')
+                option.value = x
+                option.innerText = x
+                exchangeType.appendChild(option)
+            })
+            const form = document.querySelector('#calculation')
+            form.addEventListener('submit', (e) => {
+                e.preventDefault()
+
+                const usdAmount = document.querySelector('#usdAmount').value
+                const exchangeType = document.querySelector('#exchangeType').value
+                const exchangeRate = rates.usd[exchangeType]
+                console.log(exchangeRate)
+
+                const exchangeValue = usdAmount * exchangeRate
+
+                const returnValue = document.querySelector('#returnValue')
+                const exchangeDisplay = document.createElement('p')
+                exchangeDisplay.innerText = `${exchangeValue.toFixed(2)} ${exchangeType}`
+                returnValue.appendChild(exchangeDisplay)
+            })
+        })
 }
-currencyCalculator()
+currency()
