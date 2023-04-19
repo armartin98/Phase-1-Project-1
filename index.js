2dffc96c06fffa77b1d2e0b28f4608596ad55db5
Andrew <armartin1998@gmail.com>
2023-04-19 08:07:05 -0400

    submit updated for new json

--- index.js
+++ index.js
@@ -7,39 +7,41 @@ function currency(){
             const returnValue = document.querySelector('#returnValue')
             rates.map(exchange => {
                 const option = document.createElement('option')
-                option.value = exchange["Abbreviation"]
-                option.innerText = exchange["Abbreviation"]
+                option.value = exchange.abbreviation
+                option.innerText = exchange.abbreviation
                 exchangeType.appendChild(option)
             })
-            // const form = document.querySelector('#calculation')
-            // form.addEventListener('submit', (e) => {
-            //     e.preventDefault()
+            const form = document.querySelector('#calculation')
+            form.addEventListener('submit', (e) => {
+                e.preventDefault()
 
-            //     const usdAmount = document.querySelector('#usdAmount').value
-            //     const exchangeType = document.querySelector('#exchangeType').value
-            //     const exchangeRate = rates.usd[exchangeType]
-            //     console.log(exchangeRate)
+                const usdAmount = document.querySelector('#usdAmount').value
+                const exchangeType = document.querySelector('#exchangeType').value
+                const selectedRate = rates.find(type => {
+                    if (type.abbreviation === exchangeType){
+                        console.log(type.exchangeRate)
+                        const exchangeValue = usdAmount * type.exchangeRate
 
-            //     const exchangeValue = usdAmount * exchangeRate
-
-            //     const exchangeDisplay = document.createElement('p')
-            //     exchangeDisplay.innerText = `${usdAmount} USD is equal to ${exchangeValue.toFixed(2)} ${exchangeType}`
-            //     returnValue.appendChild(exchangeDisplay)
-            // })
-            // const darkLight = document.querySelector('#darkLight')
+                        const exchangeDisplay = document.createElement('p')
+                        exchangeDisplay.innerText = `${usdAmount} USD is equal to ${exchangeValue.toFixed(2)} ${exchangeType}`
+                        returnValue.appendChild(exchangeDisplay)
+                    }
+                })
+            })
+            const darkLight = document.querySelector('#darkLight')
             
-            // darkLight.addEventListener('click', () => {
-            //     const element = document.body;
-            //     element.classList.toggle("darkMode")
-            // })
-            // document.addEventListener('keydown', (e) => {
-            //     if (e.code === 'Space') {
-            //         form.reset()
-            //         while (returnValue.firstChild){
-            //             returnValue.removeChild(returnValue.firstChild)
-            //         }
-            //     }
-            // })
+            darkLight.addEventListener('click', () => {
+                const element = document.body;
+                element.classList.toggle("darkMode")
+            })
+            document.addEventListener('keydown', (e) => {
+                if (e.code === 'Space') {
+                    form.reset()
+                    while (returnValue.firstChild){
+                        returnValue.removeChild(returnValue.firstChild)
+                    }
+                }
+            })
         })
 }
 currency()
